2af386f1a7c76581cc22a29605dd747886a7ba9e
luoxinran28 <luoxinran28@gmail.com>
2016-01-27 16:59:28 -0700

    first commit

---
+++ modules/core/server/routes/categories.server.routes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function(app) {
+    app.route('/categories').get(function (request, response) {
+        response.json([{ name: 'Beverages' }, { name: 'Condiments' }]);
+    });
+};
